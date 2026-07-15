0c1a41df27b4730dd2bc9903f7e1e8ba8391b9d7
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-07-15 20:42:06 +0530

    roboshop frontend

---
+++ 10-frontend.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.daws90saws.shop
+
+USERID=$(id -u)
+R="\e[31m"
+Y="\e[33m"
+G="\e[32m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run the script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 != 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+
+}
+
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+VALIDATE $? "Installing nginx"
+
+rm -rf /usr/share/nginx/html/* &>> $LOGS_FILE
+VALIDATE $? "Removed Default code"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOGS_FILE
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>> $LOGS_FILE
+VALIDATE $? "Downloaded and extracted frontend code"
+
+rm -rf /etc/nginx/nginx.conf
+VALIDATE $? "Removed Default conf"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copied roboshop nginx conf"
+
+systemctl restart nginx
+systemctl enable nginx &>> $LOGS_FILE
+VALIDATE $? "Enabled and restarted nginx"
